0ff4dcb03ace1e3e594eba9a2a5b39ec4c8795da
bvbogumi <77449084+bvbogumi@users.noreply.github.com>
2021-04-03 18:28:28 -0400

    Create jtestjs.js

---
+++ jtestjs.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+   $("#toggler").click(function(){
+     $(".card").toggle();
+   });
+   
+    $("#name").change(function(){
+      var name = $("#name").val();
+      $("h1").text("Hello, "+name+"!");
+    });
+    
+    $("#noun1").change(function(){
+      var noun1 = $("#noun1").val();
+      $("h2").text("You smell like a "+noun1+".");
+    
+    });
+   
+    $("#color").change(function(){
+      var color = $("#color").val();
+      var color_class = "";
+      if(color == "red") color_class = "text-danger";
+      if(color == "green") color_class="text-success";
+      
+      $("h1").removeClass();
+      if(color_class != "")
+        $("h1").addClass(color_class);
+    });
+    
+  });
