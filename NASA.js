85d15daafb5c660772ab995ba63ee489869f93ce
bvbogumi <77449084+bvbogumi@users.noreply.github.com>
2021-05-20 08:52:59 -0400

    Create NASA.js

---
+++ NASA.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var apod = $.get("https://api.nasa.gov/planetary/apod?api_key=o5RPlXYkPrndsjnmhuZsSbQDZoBdtbLhTdrpGoj4");
+        console.log(apod);    
+            
+        apod.done(function(response){
+            $("#url").attr("src", response.url);
+            $("#title").text(response.title);
+            $("#explanation").text(response.explanation);
+            $("#copyright").text(response.copyright);
+                       
+            });
+        });
+
+
